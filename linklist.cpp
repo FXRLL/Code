2f293962489d26b2c716bfca22f34d407e938b8b
FXRLL <3011286045@qq.com>
2023-11-07 12:27:40 +0800

    data structure example about linklist and stack

--- linklist.cpp
+++ linklist.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include<stdlib.h>
-typedef struct Node
+struct Node
 {
     int data;
     struct Node* next;
-}Node;
+};
 void Print(Node* head)
 {
     printf("List is: ");
---
+++ stack.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Stack
+{
+	int data;
+	struct Stack* link;
+};
+void push(Stack* &top, int number)
+{
+	struct Stack* temp = (Stack*)malloc(sizeof(struct Stack));
+	temp->data = number;
+	temp->link = top;
+	top = temp;
+}
+int pop(Stack* &top)
+{
+	int n = top->data;
+	struct Stack* temp = top;
+	top = top->link;
+	free(temp);
+	return n;
+}
+int main()
+{
+    struct Stack* top = NULL;
+    int decimalNumber, base;
+    printf("enter a decimal number: \n");
+    scanf("%d", &decimalNumber);
+    printf("enter a number(ex: 2 (01011)): \n");
+    scanf("%d", &base);
+    if (base > 36 || base == 1)
+    {
+        printf("error ");
+        system("pause");
+        return 0;
+    }
+    while (decimalNumber > 0)
+    {
+        int remainder = decimalNumber % base;
+        push(top, remainder);
+        decimalNumber /= base;
+    }
+    printf("the decimal number change to: %d",base);
+    while (top != NULL)
+    {
+        int digit = pop(top);
+        if (digit < 10)
+        {
+            printf("%d", digit);
+        }
+        else
+        {
+            printf("%c", 'A' + (digit - 10));
+        }
+    }
+    printf("\n");
+    return 0;
+}
