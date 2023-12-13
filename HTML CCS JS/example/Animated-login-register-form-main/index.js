9cfd9c77f5b9f946d8d394ee0f9336f86331ac81
FXRLL <3011286045@qq.com>
2023-12-13 17:56:03 +0800

    add example

--- HTML CCS JS/example/Animated-login-register-form-main/index.js
+++ HTML CCS JS/example/Animated-login-register-form-main/index.js
@@ -1,9 +1,9 @@
-const loginsec=document.querySelector('.login-section')
-const loginlink=document.querySelector('.login-link')
-const registerlink=document.querySelector('.register-link')
-registerlink.addEventListener('click',()=>{
+const loginsec = document.querySelector('.login-section')
+const loginlink = document.querySelector('.login-link')
+const registerlink = document.querySelector('.register-link')
+registerlink.addEventListener('click', () => {
     loginsec.classList.add('active')
 })
-loginlink.addEventListener('click',()=>{
+loginlink.addEventListener('click', () => {
     loginsec.classList.remove('active')
 })
